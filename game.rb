807347c6548c08ac3eeadb50d55df15805037c1c
ChrisProbyn <cprobyn16@gmail.com>
2019-01-15 00:32:56 +0000

    have individual questions working, still need to make overall game work

---
+++ game.rb
@@ -0,0 +1,35 @@
+require_relative  'user'
+
+player_1 = User.new("Player 1")
+player_2 = User.new("Player 2")
+
+def create_question(user)
+    num1 = rand(20) +1
+    num2 = rand(20) +1
+    answer = num1 + num2
+    puts "#{user.name}!: What does #{num1} + #{num2} equal"
+    guess = gets.chomp.to_i
+    if guess == answer
+        puts "correct"
+    else
+        
+        user.lives -= 1
+        puts "guess again"
+        puts "you have #{user.lives} lives left"
+    end
+end
+
+
+    while(player_1.lives > 0 && player_2.lives > 0)
+        flag = 1
+        current_user = nil
+        puts "--- New  Turn"
+        if flag.even?
+            current_user = player_1
+        else 
+            current_user = player_2 
+        end
+        create_question(current_user)
+        flag +=1
+    end
+
---
+++ user.rb
@@ -0,0 +1,11 @@
+class User 
+    attr_accessor :lives, :name
+    def initialize(name)
+        @name = name
+        @lives = 3
+      end
+     def remove_life 
+        @lives -= 1
+     end
+
+end
